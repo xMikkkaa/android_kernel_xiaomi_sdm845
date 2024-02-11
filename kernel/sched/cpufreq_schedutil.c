78ba89c43c279584b2dc98c0f9883727a971e01f
saikiran2001 <bjsaikiran@gmail.com>
2024-02-11 18:06:09 +0800

    cpufreq: schedutil: exponential freq selection
    cpufreq: schedutil: exponential freq selection
    
    As of currently, schedutil takes the maximum CPU frequency, multiplies
    it by 1.5 (or + freq / 2), then multiplies that by the current load
    ratio. The resulting data is a linear equation, where the maximum CPU
    frequency is selected at approximately ~66.667% load. If the load is
    anything greater, the maximum CPU frequency is still selected.
    
    The reported load for Android usage allows schedutil to make efficient
    frequency changes to maintain throughput, however, perceivable jank is
    not accounted for. Some kernel hackers opt for some form on CPU
    boosting, whether it be minimum frequency capping, schedtune boosting,
    or the use of msm_performance userspace control.
    
    As an alternative idea, schedutil could act upon it's reported load in
    an exponential fashion. Placing the load into a square root allows
    schedutil to pick higher frequencies more often, especially around the
    middle loads. At its highest point, this algorithm can boost frequency
    selection by ~1.5x. The benefit of this method over other boosting
    concepts is that the minimum and maximum selections are kept in-tact.
    This means that at 0% load, schedutil will still pick the lowest
    available frequency, and at 100% load, schedutil will still pick the
    highest available frequency that is inline with the original algorithm.
    
    We need to use int_sqrt when calculating the square root, which returns
    an integer. Unfortunately, we need a ratio in the form of an unsigned
    decimal. We need to perform the square root with a scalar and then
    divide by the square root of that scalar later to find the square root
    of the decimal.
    
    --
    squashed
    	cpufreq: schedutil: Use exponential frequency selection
    	cpufreq: schedutil: Add tunable for exponential freq selection
    	enable by default

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -29,6 +29,7 @@ struct sugov_tunables {
 	unsigned int hispeed_freq;
 	bool pl;
 	bool iowait_boost_enable;
+	bool exp_util;
 };
 
 struct sugov_policy {
@@ -188,7 +189,11 @@ static unsigned int get_next_freq(struct sugov_policy *sg_policy,
 	unsigned int freq = arch_scale_freq_invariant() ?
 				policy->cpuinfo.max_freq : policy->cur;
 
-	freq = (freq + (freq >> 2)) * util / max;
+	if (sg_policy->tunables->exp_util)
+		freq = (freq + (freq >> 2)) * int_sqrt(util * 100 / max) / 10;
+	else
+		freq = (freq + (freq >> 2)) * util / max;
+
 
 	if (freq == sg_policy->cached_raw_freq && !sg_policy->need_freq_update)
 		return sg_policy->next_freq;
@@ -736,12 +741,31 @@ static ssize_t iowait_boost_enable_store(struct gov_attr_set *attr_set,
 	return count;
 }
 
+static ssize_t exp_util_show(struct gov_attr_set *attr_set, char *buf)
+{
+	struct sugov_tunables *tunables = to_sugov_tunables(attr_set);
+
+	return scnprintf(buf, PAGE_SIZE, "%u\n", tunables->exp_util);
+}
+
+static ssize_t exp_util_store(struct gov_attr_set *attr_set, const char *buf,
+				   size_t count)
+{
+	struct sugov_tunables *tunables = to_sugov_tunables(attr_set);
+
+	if (kstrtobool(buf, &tunables->exp_util))
+		return -EINVAL;
+
+	return count;
+}
+
 static struct governor_attr up_rate_limit_us = __ATTR_RW(up_rate_limit_us);
 static struct governor_attr down_rate_limit_us = __ATTR_RW(down_rate_limit_us);
 static struct governor_attr hispeed_load = __ATTR_RW(hispeed_load);
 static struct governor_attr hispeed_freq = __ATTR_RW(hispeed_freq);
 static struct governor_attr pl = __ATTR_RW(pl);
 static struct governor_attr iowait_boost_enable = __ATTR_RW(iowait_boost_enable);
+static struct governor_attr exp_util = __ATTR_RW(exp_util);
 
 static struct attribute *sugov_attributes[] = {
 	&up_rate_limit_us.attr,
@@ -749,6 +773,7 @@ static struct attribute *sugov_attributes[] = {
 	&hispeed_load.attr,
 	&hispeed_freq.attr,
 	&pl.attr,
+	&exp_util.attr,
 	&iowait_boost_enable.attr,
 	NULL
 };
@@ -870,6 +895,7 @@ static void sugov_tunables_save(struct cpufreq_policy *policy,
 	}
 
 	cached->pl = tunables->pl;
+	cached->exp_util = tunables->exp_util;
 	cached->hispeed_load = tunables->hispeed_load;
 	cached->hispeed_freq = tunables->hispeed_freq;
 	cached->up_rate_limit_us = tunables->up_rate_limit_us;
@@ -892,6 +918,7 @@ static void sugov_tunables_restore(struct cpufreq_policy *policy)
 		return;
 
 	tunables->pl = cached->pl;
+	tunables->exp_util = cached->exp_util;
 	tunables->hispeed_load = cached->hispeed_load;
 	tunables->hispeed_freq = cached->hispeed_freq;
 	tunables->up_rate_limit_us = cached->up_rate_limit_us;
@@ -951,6 +978,7 @@ static int sugov_init(struct cpufreq_policy *policy)
 	tunables->hispeed_freq = 0;
 
 	tunables->iowait_boost_enable = true;
+	tunables->exp_util = true;
 
 	policy->governor_data = sg_policy;
 	sg_policy->tunables = tunables;
