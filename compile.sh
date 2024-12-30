70a3162aafded03d46bad8a6c0c20a27afd2cd3e
bryanyee33 <65088428+bryanyee33@users.noreply.github.com>
2024-12-30 23:20:01 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -8,7 +8,8 @@ yellow='\033[0;33m'
 red='\033[0;31m'
 nocol='\033[0m'
 
-curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -
+rmdir KernelSU
+curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -s v0.9.5
 
 # Cleanup
 rm -rf out/outputs/*
