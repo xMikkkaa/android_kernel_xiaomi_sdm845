ba0d357d29a81cf15f019f752bde6338e4f89063
bryanyee33 <65088428+bryanyee33@users.noreply.github.com>
2024-12-31 00:26:08 +0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -9,7 +9,7 @@ red='\033[0;31m'
 nocol='\033[0m'
 
 rmdir KernelSU
-curl -LSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next-susfs-4.9/kernel/setup.sh" | bash
+curl -LSs "https://raw.githubusercontent.com/rifsxd/KernelSU-Next/next-susfs-4.9/kernel/setup.sh" | bash -s next-susfs-4.9
 
 git clone https://gitlab.com/simonpunk/susfs4ksu.git --depth 1 --branch kernel-4.9
 cp -r susfs4ksu/kernel_patches/* .
