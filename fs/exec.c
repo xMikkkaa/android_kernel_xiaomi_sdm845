8ff693f39be17a61c12a4b8c08faf05d71740c53
xMikkkaa <xMikkkaa@users.noreply.github.com>
2026-09-11 18:16:16 +0800

    susfs: kernel: Fix [ksu_driver_su] being wrongly installed before su session
    - [ksu_driver_su] should be installed right after the su session, not before the su session.
    
    https://gitlab.com/simonpunk/susfs4ksu/-/commit/f2878eb5c0c9c7082212ef109b9e9e5042fd9fab
    
    Co-authored-by: simonpunk <simonpunk2016@gmail.com>
    Signed-off-by: xMikkkaa <xMikkkaa@users.noreply.github.com>

--- fs/exec.c
+++ fs/exec.c
@@ -1694,15 +1694,17 @@ extern int ksu_handle_execveat(int *fd, struct filename **filename_ptr, void *ar
 			void *envp, int *flags);
 extern int ksu_handle_execveat_sucompat(int *fd, struct filename **filename_ptr, void *argv,
 				void *envp, int *flags);
+extern int ksu_install_su_fd(void);
 
-static noinline void susfs_ksu_handle_execveat_helper(int *fd, struct filename **filename_ptr, void *argv, void *envp, int *flags)
+static noinline int susfs_ksu_handle_execveat_helper(int *fd, struct filename **filename_ptr, void *argv, void *envp, int *flags)
 {
 	if (static_branch_likely(&ksu_su_compat_enabled)) {
 		if (static_branch_unlikely(&susfs_is_sdcard_android_data_not_decrypted))
-			ksu_handle_execveat(fd, filename_ptr, argv, envp, flags);
+			return ksu_handle_execveat(fd, filename_ptr, argv, envp, flags);
 		else
-			ksu_handle_execveat_sucompat(fd, filename_ptr, argv, envp, flags);
+			return ksu_handle_execveat_sucompat(fd, filename_ptr, argv, envp, flags);
 	}
+	return -EINVAL;
 }
 #endif
 
@@ -1720,6 +1722,9 @@ static int do_execveat_common(int fd, struct filename *filename,
 	struct files_struct *displaced;
 	int retval;
 	bool is_su;
+#ifdef CONFIG_KSU_SUSFS
+	bool is_su_session = false;
+#endif // #ifdef CONFIG_KSU_SUSFS
 
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
@@ -1728,7 +1733,7 @@ static int do_execveat_common(int fd, struct filename *filename,
 	if (likely(susfs_is_current_proc_no_su()))
 		goto orig_flow;
 
-	susfs_ksu_handle_execveat_helper(&fd, &filename, &argv, &envp, &flags);
+	is_su_session = !susfs_ksu_handle_execveat_helper(&fd, &filename, &argv, &envp, &flags);
 
 orig_flow:
 #endif
@@ -1846,6 +1851,10 @@ orig_flow:
 	}
 
 	retval = exec_binprm(bprm);
+#ifdef CONFIG_KSU_SUSFS
+	if (unlikely(is_su_session && retval >= 0))
+		ksu_install_su_fd();
+#endif // #ifdef CONFIG_KSU_SUSFS
 	if (retval < 0)
 		goto out;
 
