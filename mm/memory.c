80d57984636eecc60456501c17f8037369da14a9
Alex Zhang <zhangalex@google.com>
2026-09-21 17:28:58 +0800

    mm/memory.c: make remap_pfn_range() reject unaligned addr
    commit 0c4123e3fb82d6014d0a70b52eb38153f658541c upstream.
    This function implicitly assumes that the addr passed in is page aligned.
    A non page aligned addr could ultimately cause a kernel bug in
    remap_pte_range as the exit condition in the logic loop may never be
    satisfied.  This patch documents the need for the requirement, as well as
    explicitly adds a check for it.
    
    Local fit for this tree: shape matches, applied as-is.
    
    Key Changes:
    - Reject non page aligned addr in remap_pfn_range with WARN_ON_ONCE plus -EINVAL
    
    Signed-off-by: Alex Zhang <zhangalex@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Reviewed-by: Andrew Morton <akpm@linux-foundation.org>
    Link: http://lkml.kernel.org/r/20200617233512.177519-1-zhangalex@google.com
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: Harshvardhan Jha <harshvardhan.j.jha@oracle.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Signed-off-by: xMikkkaa <xMikkkaa@users.noreply.github.com>

--- mm/memory.c
+++ mm/memory.c
@@ -1820,7 +1820,7 @@ static inline int remap_pud_range(struct mm_struct *mm, pgd_t *pgd,
 /**
  * remap_pfn_range - remap kernel memory to userspace
  * @vma: user vma to map to
- * @addr: target user address to start at
+ * @addr: target page aligned user address to start at
  * @pfn: physical address of kernel memory
  * @size: size of map area
  * @prot: page protection flags for this mapping
@@ -1836,6 +1836,9 @@ int remap_pfn_range(struct vm_area_struct *vma, unsigned long addr,
 	struct mm_struct *mm = vma->vm_mm;
 	unsigned long remap_pfn = pfn;
 	int err;
+
+	if (WARN_ON_ONCE(!PAGE_ALIGNED(addr)))
+		return -EINVAL;
 
 	/*
 	 * Physically remapped pages are special. Tell the
