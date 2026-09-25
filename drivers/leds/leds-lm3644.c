1762f60be4bfccf8d73093a3b04aeaa462acba8f
xMikkkaa <xMikkkaa@users.noreply.github.com>
2026-09-26 03:21:00 +0800

    leds: lm3644: drop dead guards around cdev_del
    Both call sites test the address of the embedded cdev struct, which
    is always true. The error path only runs after a successful cdev_add
    and remove only runs on probed devices, so call cdev_del directly.
    
    Signed-off-by: xMikkkaa <xMikkkaa@users.noreply.github.com>

--- drivers/leds/leds-lm3644.c
+++ drivers/leds/leds-lm3644.c
@@ -1123,8 +1123,7 @@ err_free_torch_classdev:
 	led_classdev_unregister(&chip->cdev_torch);
 
 err_del_cdev:
-	if (&chip->cdev)
-		cdev_del(&(chip->cdev));
+	cdev_del(&(chip->cdev));
 err_destroy_device:
 	if (chip->chr_dev)
 		device_destroy(chip->chr_class, chip->dev_num);
@@ -1175,8 +1174,7 @@ static int lm3644_remove(struct i2c_client *client)
 {
 	struct lm3644_chip_data *chip = i2c_get_clientdata(client);
 
-	if (&chip->cdev)
-		cdev_del(&(chip->cdev));
+	cdev_del(&(chip->cdev));
 
 	if (chip->chr_dev)
 		device_destroy(chip->chr_class, chip->dev_num);
